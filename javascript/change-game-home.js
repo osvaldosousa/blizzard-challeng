e9002f33baa8ee33543f97f0c03963c01b0e6dc1
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-12 23:15:37 -0300

    add prop positionBackground in infoGamesHome

--- javascript/change-game-home.js
+++ javascript/change-game-home.js
@@ -1,30 +1,35 @@
 const itemsUl = document.querySelectorAll('.icons-item');
-const homeBackground = document.querySelector('.-background');
+const homeBackground = document.querySelector('.section-home');
 
 const infoGamesHome = [
   {
     urlBackground: './images/backgrounds/diablo-quatro.png',
     title: 'Retorne à escuridão com o game Diablo IV',
     paragraph: 'O retorno de Lilith traz uma era de escuridão e sofrimento',
-    button: 'Jogue Agora'
+    button: 'Jogue Agora',
+    positionBackground: 37,
   },
   {
     urlBackground: './images/backgrounds/hearthstone-home.png',
     title: 'Novo pacote de expansão de Hearthstone',
     paragraph: 'A Horda e a Aliança se encontraram no Vale Alterac para lutar',
-    button: 'Reserve agora na pré-venda'
+    button: 'Reserve agora na pré-venda',
+    positionBackground: 65,
   },
   {
     urlBackground: './images/backgrounds/worldwarcraft-shadow-home.png',
     title: 'Desbrave as Terras Sombrias em Shadowlands! ',
     paragraph: 'O que jaz além do mundo que você conhece?',
-    button: 'Reserve agora na pré-venda'
+    button: 'Reserve agora na pré-venda',
+    positionBackground: 65,
   },
   
 ];
 
 function changeBackground(index) {
+  const postitonX = infoGamesHome[index].positionBackground
   homeBackground.style.backgroundImage = `url(${infoGamesHome[index].urlBackground})`;
+  homeBackground.style.backgroundPosition = `${postitonX}% 0%`;
 };
 
 function changeTitle(index) {
