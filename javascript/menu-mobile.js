e1f79635fe06da527ac2a9b8a2f1cd8a69f4f629
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-17 00:26:57 -0300

    apdate feature togleMenu

--- javascript/menu-mobile.js
+++ javascript/menu-mobile.js
@@ -1,10 +1,17 @@
 let scrollBlock = true;
 const menuToggle = document.querySelector('.menu-toggle');
-const headerNav = document.querySelector('.header-nav')
+const headerNav = document.querySelector('.header-nav');
+const liItems = document.querySelectorAll('.ul-item');
 
-menuToggle.addEventListener('click', () => {
+function toggleMenu() {
   document.body.style.overflow = scrollBlock ? "hidden" : "initial";
   menuToggle.classList.toggle('-open', scrollBlock);
   headerNav.classList.toggle('-open', scrollBlock);
   scrollBlock = !scrollBlock;
-})
+};
+
+menuToggle.addEventListener('click', toggleMenu)
+
+liItems.forEach(li => {
+  li.addEventListener('click', toggleMenu)
+});
